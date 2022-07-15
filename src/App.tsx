3e1c6088f2c0e0a42573d6869d4c9b5582ea0091
Douglas Coke <easyg@G-Macbook-Pro.local>
2022-07-14 20:13:00 -0400

    data-added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const apikey: any = process.env.REACT_APP_CUSTOM_KEY;
+
+interface Api {
+  genres: string[];
+  id: string;
+  title: string;
+}
+
+const App: React.FC = (): JSX.Element => {
+  const [data, setData] = React.useState<Api[]>([]);
+  const [id, setId] = React.useState<string>("");
+
+  React.useEffect(() => {
+    const getData = () =>
+      fetch(`https://code-challenge.spectrumtoolbox.com/api/movies/${id}`, {
+        headers: {
+          Authorization: apikey,
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => setData(data.data));
+    getData();
+  }, [id]);
+
   return (
     <div className="App">
-      <h1>Inital Setup</h1>
+      <h1>Container</h1>
     </div>
   );
-}
+};
 
 export default App;
